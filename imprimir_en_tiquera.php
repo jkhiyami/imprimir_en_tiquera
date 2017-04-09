26457f94d7ebd0ccec2a7a65862eab3f71e57b6c
jmka <jorgekhiyami@gmail.com>
2017-04-08 23:11:04 -0400

    Ejemplo de como Imprimir en una Tiquera con PHP, usando FPDF

---
+++ imprimir_en_tiquera.php
@@ -0,0 +1,15 @@
+<?php
+    require("../../inc/php/fpdf/fpdf.php");
+    $pdf = new FPDF("P","mm", array(120,80));
+    $pdf->AddPage();
+    $pdf->SetFont('Courier',"",8);
+    $pdf->Text(7,8, utf8_decode('----------------------------'));
+    $pdf->Text(10,12, utf8_decode('EJEMPLO DE IMPRESION'));
+    $pdf->Text(9,16, utf8_decode('EN UNA TIQUERA'));
+    $pdf->Text(12,20, utf8_decode('UTILIZANDO'));
+    $pdf->Text(22,24, utf8_decode('PHP'));
+    $pdf->Text(7,28, utf8_decode('----------------------------'));
+    $pdf->Text(7,32, 'Ced/Rif :'.utf8_decode($cedrif));
+    $pdf->Text(7,36, "IMPRESO EL DIA: ". Date("d/m/Y"));
+    $pdf->Output();
+?>
